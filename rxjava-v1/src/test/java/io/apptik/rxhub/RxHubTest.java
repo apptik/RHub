06b46a74e187d45b79b5067bf9c1d9ea9cbd6cce
Kalin Maldzhanski <djodjo@djodjo.org>
2016-09-03 15:06:51 +0200

    fix tests

--- rxjava-v1/src/test/java/io/apptik/rxhub/RxHubTest.java
+++ rxjava-v1/src/test/java/io/apptik/rxhub/RxHubTest.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.apptik.rxhub.rxjava1.RxJava1Hub;
 import rx.Observer;
 import rx.subjects.PublishSubject;
 
